f78584f218d4b2587b18f6f57093af70ccdbc596
Zidane Abrisam Latif <zidaneabrisam1428@gmail.com>
2024-02-16 10:10:53 +0700

    fdsghjk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,14 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function(){
-    return view('hello');
-});
+// Route for AdminController with Resource
 
-Route::get('/parent', function(){
-    return view('admin.parent');
-});
-
-Route::resource('/about', AboutController::class);
-
-Route::get('/profile-index', [ProfileController::class, 'index']) -> name('profilename');
+Route::resource('admin', AdminController::class);
